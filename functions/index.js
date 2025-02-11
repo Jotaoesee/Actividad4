ec878d9925528d264b7f72aaa58332b46f1fc249
Jotaoesee <jadiazm1995@gmail.com>
2025-02-11 18:47:43 +0100

    Configuración de Express y Firebase Functions

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,16 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")("sk_test_TU_CLAVE_SECRETA_STRIPE");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// Inicialización de Firebase Admin y Firestore
+admin.initializeApp();
+const db = admin.firestore();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Configuración de Express y CORS
+const app = express();
+app.use(cors({ origin: true }));
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Exportar API de Express como una función de Firebase
+exports.api = functions.https.onRequest(app);
